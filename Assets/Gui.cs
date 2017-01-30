0e755953980312df7029305d263672626f05e811
Alveeee <doubleabatteryalvarez@gmail.com>
2017-01-30 15:19:29 -0700

    Movement
    Player movement is now working.
    
    Jumping
    Players can jump off of the ground, and once more in the air (with
    reduced force)
    4 jumping stages - jump, rising, falling, and grounded
    Players will maintain their current x velocity in the air, but will not
    be able to change it
    
    Moving
    Three states - right, left, and idle
    Player's velocity is set/maintained while button is held
    Player's velocity undergoes deacceleration uppon release of move button

--- Assets/Gui.cs
+++ Assets/Gui.cs
@@ -7,9 +7,13 @@ public class Gui : MonoBehaviour {
     public float inputButtonSize = 1;
     Rect master;
     Rect[] inputButtons = new Rect[4];
+    Henry henry;
+    bool[] buttonsPressed = new bool[4];
+    public bool firstJumpFrame = true;
 
     void Start()
     {
+        henry = GameObject.FindWithTag("Character").GetComponent<Henry>();
         master = new Rect(Screen.dpi * inputButtonSize, Screen.height - (2*Screen.dpi*inputButtonSize), Screen.width - (2*Screen.dpi*inputButtonSize), 2*Screen.dpi*inputButtonSize);
         inputButtons[0] = new Rect(0, master.y, Screen.width - master.xMax, master.height);
         inputButtons[1] = new Rect(master.xMax, master.y, inputButtons[0].width, master.height);
@@ -17,12 +21,58 @@ public class Gui : MonoBehaviour {
         inputButtons[3] = new Rect(master.x, inputButtons[2].yMax, master.width, master.height/2);
 
     }
+
+    void Update()
+    {
+        if (buttonsPressed[0])
+        {
+            if (!buttonsPressed[1])
+            {
+                if (henry.myMove != MoveState.left)
+                    henry.myMove = MoveState.left;
+            }
+        }
+        else if (buttonsPressed[1])
+        {
+            if (henry.myMove != MoveState.right)
+                henry.myMove = MoveState.right;
+        }
+        else
+        {
+            if (henry.myMove != MoveState.idle)
+                henry.myMove = MoveState.idle;
+        }
+        if (buttonsPressed[2])
+        {
+            if (firstJumpFrame)
+            {
+                firstJumpFrame = false;
+                if (henry.myJump != JumpState.jump)
+                {
+                    henry.myJump = JumpState.jump;
+                }
+            }
+        }
+        else
+        {
+            if (!firstJumpFrame)
+                firstJumpFrame = true;
+        }
+    }
+
 	// Use this for initialization
     void OnGUI ()
     {
-        foreach (Rect button in inputButtons)
+        for(int i = 0; i < 4; i++)
         {
-            GUI.RepeatButton(button, "");
+            if (GUI.RepeatButton(inputButtons[i], ""))
+            {
+                if (!buttonsPressed[i])
+                    buttonsPressed[i] = true;
+            }
+            else if (buttonsPressed[i])
+                buttonsPressed[i] = false;
+
         }
     }
 }
--- Assets/Henry.cs
+++ Assets/Henry.cs
@@ -3,43 +3,90 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public enum MoveState {idle, right, left}
-public enum JumpState {grounded, jump, raising, falling}
+public enum JumpState {grounded, jump, rising, falling}
 
+public enum GameColor {Black, Grey, Grey2, White}
 public class Henry : MonoBehaviour {
 
     public MoveState myMove = MoveState.idle;
     public JumpState myJump = JumpState.grounded;
+    public bool crouched = false;
     public float runSpeed = 1;
     public float jumpForce = 1;
     public float doubleJumpMod = .5f;
+    float groundDetectBuffer = .05f;
+    public GameColor myGameColor = GameColor.Grey;
     int jump = 1;
+    public float deaccelerationMultiplyer = 1;
+    BoxCollider2D characterCollider;
+    Gui gui;
+    Devon devon;
 
     Rigidbody2D rb;
 	// Use this for initialization
 	void Start () {
         myMove = MoveState.idle;
         myJump = JumpState.grounded;
-	}
+        characterCollider = GetComponent<BoxCollider2D>();
+        rb = GetComponent<Rigidbody2D>();
+        devon = GameObject.FindWithTag("Player").GetComponent<Devon>();
+        gui = devon.GetComponent<Gui>();
+    }
 	
     void FixedUpdate()
     {
-        if (myMove == MoveState.right)
-            rb.velocity = new Vector2(runSpeed, rb.velocity.y);
-        else if (myMove == MoveState.left)
-            rb.velocity = new Vector2(-runSpeed, rb.velocity.y);
-        else
-            rb.velocity = new Vector2(0, rb.velocity.y);
-        
+        RaycastHit2D groundHit = Physics2D.Raycast(transform.TransformPoint(new Vector2(0, -characterCollider.size.y / 2 + characterCollider.offset.y + .01f)), Vector2.down, groundDetectBuffer, LayerMask.GetMask(new string[]{GameColor.Black.ToString(), myGameColor.ToString()}));
+        //Debug.DrawRay(transform.TransformPoint(new Vector2(0, -characterCollider.size.y / 2 + characterCollider.offset.y)), Vector2.down, Color.green, groundDetectBuffer);
         if (myJump == JumpState.jump)
         {
-            if (jump < 3)
+            if (jump < 3 && rb.velocity.y < 5)
             {
-                rb.AddForce(new Vector2(0, jumpForce / jump));
+                Debug.Log(rb.velocity.y);
+                rb.AddForce(new Vector2(0, jumpForce / jump), ForceMode2D.Impulse);
+                if (rb.velocity.y > 0)
+                    myJump = JumpState.rising;
+                else
+                    myJump = JumpState.falling;
                 jump++;
             }
         }
-            
-    }
+        else if (myJump == JumpState.rising)
+        {
+            if (rb.velocity.y <= 0)
+            {
+                myJump = JumpState.falling;
+            }
+        }
+        else if (myJump == JumpState.falling)
+        {
+        }
 
+        if (groundHit)
+        {
+            if (Mathf.Abs(rb.velocity.y) < .1f)
+            {
+                if (myJump != JumpState.grounded)
+                {
+                    myJump = JumpState.grounded;
+                }
 
+                if (jump > 1)
+                    jump = 1;
+            }
+         
+            if (myMove == MoveState.right)
+                rb.velocity = new Vector2(runSpeed, rb.velocity.y);
+            else if (myMove == MoveState.left)
+                rb.velocity = new Vector2(-runSpeed, rb.velocity.y);
+            else
+            {
+                if (Mathf.Abs(rb.velocity.x) > .1f)
+                    rb.velocity = new Vector2(rb.velocity.x * (1 - (deaccelerationMultiplyer / 10)), 0);
+                else
+                    rb.velocity = new Vector2(0, rb.velocity.y);
+            }
+                
+        }
+        
+     }
 }
