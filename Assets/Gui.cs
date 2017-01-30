2c2a847f8dc0f22eb961132dfe21b9b4fbf0d61b
Alveeee <doubleabatteryalvarez@gmail.com>
2017-01-30 12:36:28 -0700

    Initial Commit
    This is the first commit into this GitHub

---
+++ Assets/Gui.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gui : MonoBehaviour {
+
+    public float inputButtonSize = 1;
+    Rect master;
+    Rect[] inputButtons = new Rect[4];
+
+    void Start()
+    {
+        master = new Rect(Screen.dpi * inputButtonSize, Screen.height - (2*Screen.dpi*inputButtonSize), Screen.width - (2*Screen.dpi*inputButtonSize), 2*Screen.dpi*inputButtonSize);
+        inputButtons[0] = new Rect(0, master.y, Screen.width - master.xMax, master.height);
+        inputButtons[1] = new Rect(master.xMax, master.y, inputButtons[0].width, master.height);
+        inputButtons[2] = new Rect(master.x, master.y, master.width, master.height/2);
+        inputButtons[3] = new Rect(master.x, inputButtons[2].yMax, master.width, master.height/2);
+
+    }
+	// Use this for initialization
+    void OnGUI ()
+    {
+        foreach (Rect button in inputButtons)
+        {
+            GUI.RepeatButton(button, "");
+        }
+    }
+}
---
+++ Assets/Henry.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MoveState {idle, right, left}
+public enum JumpState {grounded, jump, raising, falling}
+
+public class Henry : MonoBehaviour {
+
+    public MoveState myMove = MoveState.idle;
+    public JumpState myJump = JumpState.grounded;
+    public float runSpeed = 1;
+    public float jumpForce = 1;
+    public float doubleJumpMod = .5f;
+    int jump = 1;
+
+    Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+        myMove = MoveState.idle;
+        myJump = JumpState.grounded;
+	}
+	
+    void FixedUpdate()
+    {
+        if (myMove == MoveState.right)
+            rb.velocity = new Vector2(runSpeed, rb.velocity.y);
+        else if (myMove == MoveState.left)
+            rb.velocity = new Vector2(-runSpeed, rb.velocity.y);
+        else
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        
+        if (myJump == JumpState.jump)
+        {
+            if (jump < 3)
+            {
+                rb.AddForce(new Vector2(0, jumpForce / jump));
+                jump++;
+            }
+        }
+            
+    }
+
+
+}
